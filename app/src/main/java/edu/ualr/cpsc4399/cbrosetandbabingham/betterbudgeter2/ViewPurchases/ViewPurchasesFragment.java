55328100d0460e23ea78259d380e07780d6de53e
ConnorRoset <croset.estem@gmail.com>
2016-11-26 15:40:33 -0600

    View Purchase RV set up

--- app/src/main/java/edu/ualr/cpsc4399/cbrosetandbabingham/betterbudgeter2/ViewPurchases/ViewPurchasesFragment.java
+++ app/src/main/java/edu/ualr/cpsc4399/cbrosetandbabingham/betterbudgeter2/ViewPurchases/ViewPurchasesFragment.java
@@ -2,10 +2,13 @@ package edu.ualr.cpsc4399.cbrosetandbabingham.betterbudgeter2.ViewPurchases;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import edu.ualr.cpsc4399.cbrosetandbabingham.betterbudgeter2.MainActivity;
 import edu.ualr.cpsc4399.cbrosetandbabingham.betterbudgeter2.R;
 
 /**
@@ -13,6 +16,9 @@ import edu.ualr.cpsc4399.cbrosetandbabingham.betterbudgeter2.R;
  */
 
 public class ViewPurchasesFragment extends Fragment {
+
+    RecyclerView recyclerView;
+    ViewPurchasesRVAdapter adapter;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -25,5 +31,10 @@ public class ViewPurchasesFragment extends Fragment {
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        recyclerView = (RecyclerView)getActivity().findViewById(R.id.view_purchases_list_of_purchases_recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        adapter = new ViewPurchasesRVAdapter(((MainActivity)getActivity()).getBudget().getPurchases());
+        recyclerView.setAdapter(adapter);
     }
 }
---
+++ app/src/main/java/edu/ualr/cpsc4399/cbrosetandbabingham/betterbudgeter2/ViewPurchases/ViewPurchasesRVAdapter.java
@@ -0,0 +1,73 @@
+package edu.ualr.cpsc4399.cbrosetandbabingham.betterbudgeter2.ViewPurchases;
+
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.ToggleButton;
+
+import java.util.List;
+
+import edu.ualr.cpsc4399.cbrosetandbabingham.betterbudgeter2.BudgetContents.Purchase;
+import edu.ualr.cpsc4399.cbrosetandbabingham.betterbudgeter2.R;
+
+/**
+ * Created by connorroset on 11/26/16.
+ */
+
+public class ViewPurchasesRVAdapter extends RecyclerView.Adapter<ViewPurchasesRVAdapter.ViewPurchasesViewHolder> {
+
+    public static class ViewPurchasesViewHolder extends RecyclerView.ViewHolder{
+
+        CardView cv;
+        TextView type;
+        TextView amount;
+        TextView paid;
+        Button delete;
+        ViewPurchasesViewHolder(View itemView) {
+            super(itemView);
+            cv = (CardView) itemView.findViewById(R.id.card_view);
+            type = (TextView) itemView.findViewById(R.id.type_card_view);
+            amount = (TextView) itemView.findViewById(R.id.amount_card_view);
+            paid = (ToggleButton) itemView.findViewById(R.id.paid_card_view);
+            delete = (Button) itemView.findViewById(R.id.delete_button_card_view);
+
+
+        }
+    }
+
+    private List <Purchase> purchases;
+    public ViewPurchasesRVAdapter(List<Purchase> purchases){this.purchases = purchases;}
+
+    @Override
+    public int getItemCount(){ return purchases.size();}
+
+    @Override
+    public ViewPurchasesViewHolder onCreateViewHolder(ViewGroup viewGroup, int i){
+        View v = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.card_layout, viewGroup, false);
+        ViewPurchasesViewHolder viewPurchasesViewHolder = new ViewPurchasesViewHolder(v);
+        return viewPurchasesViewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(final ViewPurchasesViewHolder viewPurchasesViewHolder, final int i) {
+        viewPurchasesViewHolder.type.setText(purchases.get(i).getType());
+        viewPurchasesViewHolder.amount.setText(String.valueOf(purchases.get(i).getAmount()));
+//        viewPurchasesViewHolder.paid.setText("");
+        viewPurchasesViewHolder.paid.setVisibility(View.INVISIBLE);
+        viewPurchasesViewHolder.delete.setVisibility(View.INVISIBLE);
+//        viewPurchasesViewHolder.delete.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                purchases.remove(viewPurchasesViewHolder.getAdapterPosition());
+//                notifyDataSetChanged();
+//            }
+//        });
+
+
+    }
+
+}
